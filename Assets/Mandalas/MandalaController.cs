486925f3de8a92c06fb36debbf5689dbfca3837c
uberyoji <jimmy.beaubien@gmail.com>
2019-05-13 20:31:04 -0400

    Update webapp
    New touch controls to change point count

--- Assets/Mandalas/MandalaController.cs
+++ Assets/Mandalas/MandalaController.cs
@@ -10,15 +10,15 @@ public class MandalaController : MonoBehaviour
     private Vector2 LastPos = new Vector2();
     private Vector2 DeltaPos = new Vector2();
 
-    private float Iterations = 1;
-    public float IterationFactor = 1000f;
+    private float PC = 1;
+    public float YFactor = 100f;
 
     void Start()
     {
         if (Camera.main.pixelHeight > Camera.main.pixelWidth)
             Camera.main.orthographicSize = 2;
 
-        Iterations = Mandala.Iterations;
+        PC = (float)Mandala.PointCount;
     }
 
     void Update()
@@ -32,23 +32,16 @@ public class MandalaController : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
             CurrPos.Set(Input.mousePosition.x / Camera.main.pixelWidth, (Camera.main.pixelHeight - Input.mousePosition.y) / Camera.main.pixelHeight);
-
-            //            Debug.Log("Mouse Position CX=" + CurrPos + " LX="+LastPos);
-
             DeltaPos.Set(LastPos.x - CurrPos.x, LastPos.y - CurrPos.y);
-
-//            if ( Mathf.Abs(LastPos.x) > Mathf.Abs(LastPos.y) )
             {
                 Mandala.Ratio += DeltaPos.x;
                 Mandala.Ratio = Mathf.Repeat(Mandala.Ratio, 1f);
             }
-//            else
             {
-                Iterations += DeltaPos.y * IterationFactor;
-                Iterations = Mathf.Clamp(Iterations, 0, 50);
-                Mandala.Iterations = (int)Iterations;
+                PC += (DeltaPos.y * YFactor);
+                PC = Mathf.Clamp(PC, 3, 16);
+                Mandala.PointCount = (int)PC;
             }
-
             LastPos.Set(CurrPos.x, CurrPos.y);
         }
     }
