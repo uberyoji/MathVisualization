d8d419e996013f9bd877423204594f8bce43b139
uberyoji <jimmy.beaubien@gmail.com>
2019-07-22 13:31:23 -0400

    Adding new fractal flower base
    Initial release for this new fractal.
    Also moved around orbit camera.

---
+++ Assets/FractalFlower/FractalFlower.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FractalFlower : MonoBehaviour
+{
+    public int PetalPerIteration = 6;
+    public float ThetaRatioPerIteration = 0.5f;
+    private float ThetaIteration = 0f;
+
+    public float InterIterationDelay = 0.5f; // in seconds
+    private float NextIterationTime = 0f;
+    
+    public GameObject PetalPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    GameObject GetNextFreePetal(float Base, float Angle)
+    {        
+        transform.eulerAngles = new Vector3(0, 0, Base+Angle);       // fixme optim
+
+        return GameObject.Instantiate(PetalPrefab, transform.position, transform.rotation); // fixme optim
+    }
+
+    void SpawnPetals()
+    {
+        float a = 0f;
+        float da = 360 / PetalPerIteration;
+
+        GameObject Petal;
+
+        transform.eulerAngles = new Vector3(0f, 0f, ThetaIteration);
+
+        for (int i = 0; i < PetalPerIteration; i++)
+        {
+            Petal = GetNextFreePetal(ThetaIteration,a);
+            a += da;
+        }
+
+        ThetaIteration += (360f * ThetaRatioPerIteration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (NextIterationTime <= Time.time)
+        {
+            SpawnPetals();
+            NextIterationTime = NextIterationTime + InterIterationDelay;
+        }
+    }
+}
---
+++ Assets/FractalFlower/Petal.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Petal : MonoBehaviour
+{
+    public float RemoveAfter = 1.0f;
+    private float DeathTime;
+
+    public float Speed = 1f;
+
+    private Vector3 Position = new Vector3();
+    private Vector3 Velocity = new Vector3();
+
+    public AnimationCurve WidthOverTime;
+    public LineRenderer LR;
+    public float BaseLineWidth = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DeathTime = Time.time + RemoveAfter;
+
+        transform.position = Position;
+
+        Velocity.Set(Mathf.Cos( Mathf.Deg2Rad * transform.eulerAngles.z) * Speed, Mathf.Sin( Mathf.Deg2Rad * transform.eulerAngles.z) * Speed, 0f);
+
+        float Scale = WidthOverTime.Evaluate(0f);
+        transform.localScale = new Vector3(Scale, Scale, 1f);
+
+        LR.widthMultiplier = Scale * BaseLineWidth;
+
+        float C = Mathf.Cos( Mathf.Deg2Rad * 60f );
+        float S = Mathf.Sin(Mathf.Deg2Rad * 60f);
+
+        LR.SetPosition(0, new Vector3(-C, S,0f));
+        LR.SetPosition(2, new Vector3(-C, -S, 0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(DeathTime <= Time.time)
+        {
+            GameObject.Destroy(gameObject);
+        }
+        else
+        {
+            float Eval = (DeathTime - Time.time) / RemoveAfter;
+            Eval = Mathf.Clamp(1f - Eval, 0f, 1f);
+
+            float Scale = WidthOverTime.Evaluate(Eval);
+
+            LR.widthMultiplier = Scale * BaseLineWidth;
+            
+            transform.localScale = new Vector3(Scale, Scale, 1f);
+
+            Position += Velocity * Time.deltaTime;
+            transform.position = Position;
+        }
+    }
+}
